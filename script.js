aa84fcea4ab207307d894eeb86b5534cb98e7c9e
Krish Khatiwada <krishkhatiwada125@gmail.com>
2025-05-12 15:11:39 +0545

    first commit

--- script.js
+++ script.js
@@ -1,23 +1,63 @@
+// let n = Math.random();
+// console.log (n)
+
+// let a = prompt("Enter first number");
+// let b = prompt("Enter operation");
+// let c = prompt("Enter second number");
+
+
+// const obj = {
+//     "+": "-",
+//     "*": "+",
+//     "-": "/",
+//     "/": "**"
+// }
+
+// if (n > 0.1) {
+//     alert(`The result is ${eval(`${a} ${b} ${c}`)}`);
+// }
+
+// else {
+//     b = obj[b];
+//     alert(`The result is ${eval(`${a} ${b} ${c}`)}`);
+// }
+
+
+// Bussiness name generator
+
 let n = Math.random();
-console.log (n)
+let first, second, third;
 
-let a = prompt("Enter first number");
-let b = prompt("Enter operation");
-let c = prompt("Enter second number");
-
-
-const obj = {
-    "+": "-",
-    "*": "+",
-    "-": "/",
-    "/": "**"
+if (n < 0.33) {
+    first = "Crazy"
 }
-
-if (n > 0.1) {
-    alert(`The result is ${eval(`${a} ${b} ${c}`)}`);
+else if (n >= 0.33 && n < 0.66) {
+    first = "Amazing"
 }
-
 else {
-    b = obj[b];
-    alert(`The result is ${eval(`${a} ${b} ${c}`)}`);
+    first = "Fire"
 }
+
+n = Math.random();
+if (n < 0.33) {
+    second = "Engine"
+}
+else if (n >= 0.33 && n < 0.66) {
+    second = "Foods"
+}
+else {
+    second = "Garments"
+}
+
+n = Math.random();
+if (n < 0.33) {
+    third = "Bros"
+}
+else if (n >= 0.33 && n < 0.66) {
+    third = "Limited"
+}
+else {
+    third = "Hub"
+}
+
+console.log(`${first} ${second} ${third}`);
